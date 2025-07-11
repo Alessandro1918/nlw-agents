af58a8f6ec9e73d92fb40ef010edd51383a27c4a
Alessandro1918 <alessandro1918@hotmail.com>
2025-07-10 22:35:23 -0300

    feat: back route to transcribe input audio and compare to questions embeddings to generate an answer

--- back/src/http/routes/create-question.ts
+++ back/src/http/routes/create-question.ts
@@ -1,7 +1,9 @@
+import { and, eq, sql } from 'drizzle-orm'
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import { z } from 'zod/v4'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
+import { generateAnswer, generateEmbeddings } from '../../services/gemini.ts'
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -20,10 +22,41 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
       const { roomId } = request.params
       const { question } = request.body
 
+      const questionEmbeddings = await generateEmbeddings(question)
+
+      const questionEmbeddingsAsString = `[${questionEmbeddings.join(',')}]`
+
+      // Get the top 3 best audio chunks with similarity with question > 70%
+      const chunks = await db
+        .select({
+          id: schema.audioChunks.id,
+          transcription: schema.audioChunks.transcription,
+          similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${questionEmbeddingsAsString}::vector)`,
+        })
+        .from(schema.audioChunks)
+        .where(
+          and(
+            eq(schema.audioChunks.roomId, roomId),
+            sql`1 - (${schema.audioChunks.embeddings} <=> ${questionEmbeddingsAsString}::vector) > 0.7`
+          )
+        )
+        .orderBy(
+          sql`${schema.audioChunks.embeddings} <=> ${questionEmbeddingsAsString}::vector`
+        )
+        .limit(3)
+
+      let answer: string | null = null
+
+      if (chunks.length > 0) {
+        const transcriptions = chunks.map((chunk) => chunk.transcription)
+
+        answer = await generateAnswer(question, transcriptions)
+      }
+
       const result = await db
         .insert(schema.questions)
-        .values({ roomId, question })
-        .returning()
+        .values({ roomId, question, answer })
+        .returning()  // postgres default bahavior returns the inserted lines count. This "returning" makes it return the inserted line
 
       const insertedQuestion = result[0]
 
@@ -31,7 +64,10 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
         throw new Error('Failed to create new room.')
       }
 
-      return reply.status(201).send({ questionId: insertedQuestion.id })
+      return reply.status(201).send({
+        questionId: insertedQuestion.id,
+        answer,
+      })
     }
   )
 }
